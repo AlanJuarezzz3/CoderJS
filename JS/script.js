75ef1651a3b01e3a58ed91540070e41a81a8f269
ALan Juarez <alan.juarez_roman@hotmail.com>
2022-08-03 20:48:07 -0300

    primer pre entrega

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,7 @@
+
 let notas;
 let promedio;
 let suma = 0;
-
-const saludar = () =>{
-    console.log("hola soy una funcion");
-}
 
 let nombres = prompt("ingrese su nombre");
 let apellidos = prompt("ingrese su apellido");
@@ -34,3 +31,35 @@ else {
 
 
 
+class Alumno{
+    constructor(nombre, materia, nota, ubicacion, id){
+        this.nombre = nombre;
+        this.materia = materia;
+        this.nota = parseInt(nota);
+        this.ubicacion = ubicacion;
+        this.id = id;
+    }
+    asignarId(array){
+        this.id=array.length;
+    }
+}
+
+const alumnos = [
+    new Alumno("Alan", "Matematica", 10, "Buenos Aires", 01),
+    new Alumno("Lucia", "Matematica", 7, "Buenos Aires", 02)
+]
+
+let continuar = true;
+
+while(continuar){
+    let ingreso = prompt("ingrese datos del alumno:nombre, materia, nota y ubicacion,separado con(/). X para finalizar");
+    if (ingreso.toUpperCase()=="X"){
+        continuar=false;
+        break;
+    }
+    let datos = ingreso.split("/");
+    const alumno = new Alumno (datos [0],datos[1], datos[2], datos[3]);
+
+    alumnos.push(alumno);
+    alumno.asignarId(alumnos);
+}
