206013474e35a1c52c8c115a3a9fa9797d30e88e
ALan Juarez <alan.juarez_roman@hotmail.com>
2022-08-07 17:00:22 -0300

    DOM

--- JS/script.js
+++ JS/script.js
@@ -1,65 +1,56 @@
+let datos = [];
 
-let notas;
-let promedio;
-let suma = 0;
+function registrar(){
+    let nombre = document.getElementById('alumno').value;
+    let asignatura = document.getElementById('asignatura').value;
+    let p1 = document.getElementById('p1').value;
+    let p2 = document.getElementById('p2').value;
+    
 
-let nombres = prompt("ingrese su nombre");
-let apellidos = prompt("ingrese su apellido");
-alert(nombres + " " + apellidos);
+    datos.push({
+        'alumno': nombre,
+        'asignatura': asignatura,
+        'parcial1':p1,
+        'parcial2':p2,
+        'promedio': (parseInt(p1) + parseInt(p2)) /2,
+        'condicion':promedios(p1,p2),
+    })
 
-let asignatura = prompt("indique asignatura");
-alert(asignatura);
+    document.getElementById('formCalif').reset();
+    alert('datos guardados');
+    crearTabla();
+    cerrarModal();
+   
+    function crearTabla(){
+        let fila ='';
 
-let nnotas = parseInt(prompt("indique cantidad de notas a promediar"));
-
-for (i = 0; i < nnotas; i++) {
-    let notas = parseInt(prompt("ingrese nota " + i));
-    suma = suma + notas;
-}
-
-promedio = suma / nnotas;
-alert("Su promedio en " + asignatura + " es " + promedio);
-
-if (promedio >= 7) {
-    alert("promociona");
-} else if (promedio >= 4) {
-    alert("aprobado");
-}
-else {
-    alert("recursa");
-}
-
-
-
-class Alumno{
-    constructor(nombre, materia, nota, ubicacion, id){
-        this.nombre = nombre;
-        this.materia = materia;
-        this.nota = parseInt(nota);
-        this.ubicacion = ubicacion;
-        this.id = id;
-    }
-    asignarId(array){
-        this.id=array.length;
+        datos.forEach((item, i) => {
+            fila += `<tr>
+                        <td>${item.asignatura}</td>
+                        <td>${item.parcial1}</td>
+                        <td>${item.parcial2}</td>
+                        <td>${item.promedio}</td>
+                        <td>${item.condicion}</td>
+                    </tr>`
+        })
+        document.getElementById('filas').innerHTML = fila;
     }
 }
-
-const alumnos = [
-    new Alumno("Alan", "Matematica", 10, "Buenos Aires", 01),
-    new Alumno("Lucia", "Matematica", 7, "Buenos Aires", 02)
-]
-
-let continuar = true;
-
-while(continuar){
-    let ingreso = prompt("ingrese datos del alumno:nombre, materia, nota y ubicacion,separado con(/). X para finalizar");
-    if (ingreso.toUpperCase()=="X"){
-        continuar=false;
-        break;
+function promedios(p1,p2) {
+    if ((parseInt(p1) + parseInt(p2)) /2 >= 7) {
+        return"Promociona";
+    } else if ((parseInt(p1) + parseInt(p2)) /2 >= 4) {
+        return"Final";
     }
-    let datos = ingreso.split("/");
-    const alumno = new Alumno (datos [0],datos[1], datos[2], datos[3]);
+    else {
+        return"Recursa";
+    }
+}
 
-    alumnos.push(alumno);
-    alumno.asignarId(alumnos);
+function mostrarModal(){
+    document.getElementById('modal-registro').classList.add('active');
+}
+
+function cerrarModal(){
+    document.getElementById('modal-registro').classList.remove('active');
 }
